636537706226d60508df9c1297a54117a9b7dacf
deadman96385 <seanhoyt963@gmail.com>
2019-01-24 03:47:47 -0600

    Add led driver changes
    We use sn3191
    
    Pixi4554G uses 3721SNKK2C

--- drivers/leds/leds-gpio-flash-3721s.c
+++ drivers/leds/leds-gpio-flash-3721s.c
@@ -23,7 +23,7 @@
 #include <linux/list.h>
 #include <linux/pinctrl/consumer.h>
 
-/* #define CONFIG_GPIO_FLASH_DEBUG */
+//#define CONFIG_GPIO_FLASH_DEBUG 
 #undef CDBG
 #ifdef CONFIG_GPIO_FLASH_DEBUG
 #define CDBG(fmt, args...) pr_err(fmt, ##args)
@@ -31,7 +31,7 @@
 #define CDBG(fmt, args...) do { } while (0)
 #endif
 
-#define LED_GPIO_FLASH_DRIVER_NAME	"qcom,leds-gpio-flash"
+#define LED_GPIO_FLASH_DRIVER_NAME	"camera-led-3721s"
 #define LED_TRIGGER_DEFAULT		"none"
 
 #define GPIO_OUT_LOW          (0 << 1)
@@ -39,7 +39,7 @@
 
 enum msm_flash_seq_type_t {
 	FLASH_EN,
-	FLASH_NOW,
+	//FLASH_NOW,
 };
 
 struct msm_flash_ctrl_seq {
@@ -51,12 +51,12 @@ struct msm_flash_ctrl_seq {
 
 struct led_gpio_flash_data {
 	int flash_en;
-	int flash_now;
+	//int flash_now;
 	int brightness;
 	struct led_classdev cdev;
 	struct pinctrl *pinctrl;
 	struct pinctrl_state *gpio_state_default;
-	struct msm_flash_ctrl_seq ctrl_seq[2];
+	struct msm_flash_ctrl_seq ctrl_seq[2];                                   
 };
 
 static struct of_device_id led_gpio_flash_of_match[] = {
@@ -72,34 +72,25 @@ static void led_gpio_brightness_set(struct led_classdev *led_cdev,
 	    container_of(led_cdev, struct led_gpio_flash_data, cdev);
 
 	int brightness = value;
-	int flash_en = 0, flash_now = 0;
+	int flash_en = 0;
 
 	if (brightness > LED_HALF) {
 		flash_en =
 			flash_led->ctrl_seq[FLASH_EN].flash_on_val;
-		flash_now =
-			flash_led->ctrl_seq[FLASH_NOW].flash_on_val;
+	
 	} else if (brightness > LED_OFF) {
 		flash_en =
 			flash_led->ctrl_seq[FLASH_EN].torch_on_val;
-		flash_now =
-			flash_led->ctrl_seq[FLASH_NOW].torch_on_val;
+
 	} else {
 		flash_en = 0;
-		flash_now = 0;
 	}
-	CDBG("%s:flash_en=%d, flash_now=%d\n", __func__, flash_en, flash_now);
+	CDBG("%s:flash_en=%d, \n", __func__, flash_en);
 
 	rc = gpio_direction_output(flash_led->flash_en, flash_en);
 	if (rc) {
 		pr_err("%s: Failed to set gpio %d\n", __func__,
 		       flash_led->flash_en);
-		goto err;
-	}
-	rc = gpio_direction_output(flash_led->flash_now, flash_now);
-	if (rc) {
-		pr_err("%s: Failed to set gpio %d\n", __func__,
-		       flash_led->flash_now);
 		goto err;
 	}
 	flash_led->brightness = brightness;
@@ -115,7 +106,7 @@ static enum led_brightness led_gpio_brightness_get(struct led_classdev
 	return flash_led->brightness;
 }
 
-int led_gpio_flash_probe(struct platform_device *pdev)
+int led_3721s_gpio_flash_probe(struct platform_device *pdev)
 {
 	int rc = 0;
 	const char *temp_str;
@@ -176,29 +167,14 @@ int led_gpio_flash_probe(struct platform_device *pdev)
 		}
 	}
 
-	flash_led->flash_now = of_get_named_gpio(node, "qcom,flash-now", 0);
-	if (flash_led->flash_now < 0) {
-		dev_err(&pdev->dev,
-			"Looking up %s property in node %s failed. rc =  %d\n",
-			"flash-now", node->full_name, flash_led->flash_now);
-		goto error;
-	} else {
-		rc = gpio_request(flash_led->flash_now, "FLASH_NOW");
-		if (rc) {
-			dev_err(&pdev->dev,
-				"%s: Failed to request gpio %d,rc = %d\n",
-				__func__, flash_led->flash_now, rc);
-			goto error;
-		}
-	}
-
 	rc = of_property_read_string(node, "linux,name", &flash_led->cdev.name);
 	if (rc) {
 		dev_err(&pdev->dev, "%s: Failed to read linux name. rc = %d\n",
 			__func__, rc);
 		goto error;
 	}
-
+	
+/*
 	rc = of_property_read_u32_array(node, "qcom,flash-seq-val",
 		array_flash_seq, 2);
 
@@ -216,8 +192,9 @@ int led_gpio_flash_probe(struct platform_device *pdev)
 			__func__, __LINE__);
 		goto error;
 	}
+*/
 
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < 1; i++) {
 		rc = of_property_read_string_index(node,
 			"qcom,op-seq", i,
 			&seq_name);
@@ -245,25 +222,7 @@ int led_gpio_flash_probe(struct platform_device *pdev)
 			else
 				flash_led->ctrl_seq[FLASH_EN].torch_on_val =
 					GPIO_OUT_HIGH;
-		} else if (!strcmp(seq_name, "flash_now")) {
-			flash_led->ctrl_seq[FLASH_NOW].seq_type =
-				FLASH_NOW;
-			CDBG("%s:%d seq_type[%d] %d\n", __func__, __LINE__,
-				i, flash_led->ctrl_seq[i].seq_type);
-			if (array_flash_seq[i] == 0)
-				flash_led->ctrl_seq[FLASH_NOW].flash_on_val =
-					GPIO_OUT_LOW;
-			else
-				flash_led->ctrl_seq[FLASH_NOW].flash_on_val =
-					GPIO_OUT_HIGH;
-
-			if (array_torch_seq[i] == 0)
-				flash_led->ctrl_seq[FLASH_NOW].torch_on_val =
-					GPIO_OUT_LOW;
-			 else
-				flash_led->ctrl_seq[FLASH_NOW].torch_on_val =
-					GPIO_OUT_HIGH;
-		}
+		} 
 
 	}
 
@@ -288,7 +247,7 @@ error:
 	return rc;
 }
 
-int led_gpio_flash_remove(struct platform_device *pdev)
+int led_3721s_gpio_flash_remove(struct platform_device *pdev)
 {
 	struct led_gpio_flash_data *flash_led =
 	    (struct led_gpio_flash_data *)platform_get_drvdata(pdev);
@@ -300,8 +259,8 @@ int led_gpio_flash_remove(struct platform_device *pdev)
 }
 
 static struct platform_driver led_gpio_flash_driver = {
-	.probe = led_gpio_flash_probe,
-	.remove = led_gpio_flash_remove,
+	.probe = led_3721s_gpio_flash_probe,
+	.remove = led_3721s_gpio_flash_remove,
 	.driver = {
 		   .name = LED_GPIO_FLASH_DRIVER_NAME,
 		   .owner = THIS_MODULE,
